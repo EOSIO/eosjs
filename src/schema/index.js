8bf47a6deae0c10a360e8d6e68f839910afb9bda
James Calfee <james@jcalfee.info>
2018-06-01 13:31:15 -0500

    Update eosio token and system ABIs #157

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,6 +1,11 @@
-module.exports = Object.assign(
+const schema = Object.assign(
   {},
   require('./chain_types.json'),
   require('./eosio_system.json'),
   require('./eosio_token.json')
 )
+
+// change "bytes" to "abi_def"
+schema.setabi.fields.abi = 'abi_def'
+
+module.exports = schema
