c946a0a32a312552714d2075de039810d44360b7
Todd Fleming <todd.fleming@block.one>
2018-08-23 17:21:07 -0400

    typedoc

--- src/eosjs2-api.ts
+++ src/eosjs2-api.ts
@@ -10,45 +10,139 @@ const transactionAbi = require('../src/transaction.abi.json');
 
 export const serialize = ser;
 
+/**
+ * Arguments to `getRequiredKeys`
+ */
 export interface AuthorityProviderArgs {
+  /**
+   * Transaction that needs to be signed
+   */
   transaction: any;
+
+  /**
+   * Public keys associated with the private keys that the `SignatureProvider` holds
+   */
   availableKeys: string[];
 }
 
+/**
+ * Get subset of `availableKeys` needed to meet authorities in `transaction`
+ */
 export interface AuthorityProvider {
+  /**
+   * Get subset of `availableKeys` needed to meet authorities in `transaction`
+   */
   getRequiredKeys: (args: AuthorityProviderArgs) => Promise<string[]>;
 }
 
+/**
+ * Arguments to `sign`
+ */
 export interface SignatureProviderArgs {
+  /**
+   * Chain transaction is for
+   */
   chainId: string;
+
+  /**
+   * Public keys associated with the private keys needed to sign the transaction
+   */
   requiredKeys: string[];
+
+  /**
+   * Transaction to sign
+   */
   serializedTransaction: Uint8Array;
+
+  /**
+   * ABIs for all contracts with actions included in `serializedTransaction`
+   */
   abis: GetAbiResult[];
 }
 
+/**
+ * Signs transactions
+ */
 export interface SignatureProvider {
+  /**
+   * Public keys associated with the private keys that the `SignatureProvider` holds
+   */
   getAvailableKeys: () => Promise<string[]>;
+
+  /**
+   * Sign a transaction
+   */
   sign: (args: SignatureProviderArgs) => Promise<string[]>;
 }
 
+/**
+ * Holds a fetched abi
+ */
 export interface CachedAbi {
+  /**
+   * abi in binary form
+   */
   rawAbi: Uint8Array;
+
+  /**
+   * abi in structured form
+   */
   abi: Abi;
 }
 
 export class Api {
+  /**
+   * Issues RPC calls
+   */
   rpc: JsonRpc;
+
+  /**
+   * Get subset of `availableKeys` needed to meet authorities in a `transaction`
+   */
   authorityProvider: AuthorityProvider;
+
+  /**
+   * Signs transactions
+   */
   signatureProvider: SignatureProvider;
+
+  /**
+   * Identifies chain
+   */
   chainId: string;
+
   textEncoder: TextEncoder;
   textDecoder: TextDecoder;
 
+  /**
+   * Converts abi files between binary and structured form (abi.abi.json)
+   */
   abiTypes: Map<string, ser.Type>;
+
+  /**
+   * Converts transactions between binary and structured form (transaction.abi.json)
+   */
   transactionTypes: Map<string, ser.Type>;
+
+  /**
+   * Holds information needed to serialize contract actions
+   */
   contracts = new Map<string, ser.Contract>();
+
+  /**
+   * Fetched abis
+   */
   cachedAbis = new Map<string, CachedAbi>();
 
+  /**
+   * @param args 
+   *    * `rpc`: Issues RPC calls
+   *    * `authorityProvider`: Get public keys needed to meet authorities in a transaction
+   *    * `signatureProvider`: Signs transactions
+   *    * `chainId`: Identifies chain
+   *    * `textEncoder`: `TextEncoder` instance to use. Pass in `null` if running in a browser
+   *    * `textDecoder`: `TextDecider` instance to use. Pass in `null` if running in a browser
+   */
   constructor(args: {
     rpc: JsonRpc, authorityProvider?: AuthorityProvider,
     signatureProvider: SignatureProvider,
@@ -67,6 +161,9 @@ export class Api {
     this.transactionTypes = ser.getTypesFromAbi(ser.createInitialTypes(), transactionAbi);
   }
 
+  /**
+   * Get abi in both binary and structured forms. Fetch when needed.
+   */
   async getCachedAbi(accountName: string, reload = false): Promise<CachedAbi> {
     if (!reload && this.cachedAbis.get(accountName))
       return this.cachedAbis.get(accountName);
@@ -87,10 +184,16 @@ export class Api {
     return cachedAbi;
   }
 
+  /**
+   * Get abi in structured form. Fetch when needed.
+   */
   async getAbi(accountName: string, reload = false): Promise<Abi> {
     return (await this.getCachedAbi(accountName, reload)).abi;
   }
 
+  /**
+   * Get abis needed by a transaction
+   */
   async getTransactionAbis(transaction: any, reload = false): Promise<GetAbiResult[]> {
     const accounts: string[] = transaction.actions.map((action: ser.Action): string => action.account);
     const uniqueAccounts: Set<string> = new Set(accounts);
@@ -99,6 +202,9 @@ export class Api {
     return Promise.all(actionPromises);
   }
 
+  /**
+   * Get data needed to serialize actions in a contract
+   */
   async getContract(accountName: string, reload = false): Promise<ser.Contract> {
     if (!reload && this.contracts.get(accountName))
       return this.contracts.get(accountName);
@@ -112,14 +218,23 @@ export class Api {
     return result;
   }
 
+  /**
+   * Convert `value` to binary form. `type` must be a built-in abi type or in `transaction.abi.json`.
+   */
   serialize(buffer: ser.SerialBuffer, type: string, value: any): void {
     this.transactionTypes.get(type).serialize(buffer, value);
   }
 
+  /**
+   * Convert data in `buffer` to structured form. `type` must be a built-in abi type or in `transaction.abi.json`.
+   */
   deserialize(buffer: ser.SerialBuffer, type: string): any {
     return this.transactionTypes.get(type).deserialize(buffer);
   }
 
+  /**
+   * Convert a transaction to binary
+   */
   serializeTransaction(transaction: any): Uint8Array {
     let buffer = new ser.SerialBuffer({ textEncoder: this.textEncoder, textDecoder: this.textDecoder });
     this.serialize(buffer, 'transaction', {
@@ -134,12 +249,18 @@ export class Api {
     return buffer.asUint8Array();
   }
 
+  /**
+   * Convert a transaction from binary. Leaves actions in hex.
+   */
   deserializeTransaction(transaction: Uint8Array): any {
     const buffer = new ser.SerialBuffer({ textEncoder: this.textEncoder, textDecoder: this.textDecoder });
     buffer.pushArray(transaction)
     return this.deserialize(buffer, 'transaction');
   }
 
+  /**
+   * Convert actions to hex
+   */
   async serializeActions(actions: ser.Action[]): Promise<ser.SerializedAction[]> {
     return await Promise.all(actions.map(async ({ account, name, authorization, data }) => {
       const contract = await this.getContract(account)
@@ -147,6 +268,9 @@ export class Api {
     }));
   }
 
+  /**
+   * Convert actions from hex
+   */
   async deserializeActions(actions: ser.Action[]): Promise<ser.Action[]> {
     return await Promise.all(actions.map(async ({ account, name, authorization, data }) => {
       const contract = await this.getContract(account)
@@ -154,6 +278,9 @@ export class Api {
     }));
   }
 
+  /**
+   * Convert a transaction from binary. Also deserializes actions.
+   */
   async deserializeTransactionWithActions(transaction: Uint8Array | string): Promise<any> {
     if (typeof transaction === "string") {
       transaction = ser.hexToUint8Array(transaction)
@@ -168,6 +295,14 @@ export class Api {
     return !!(expiration && ref_block_num && ref_block_prefix);
   }
 
+  /**
+   * Create and optionally broadcast a transaction.
+   * 
+   * Named Parameters:
+   *    * `broadcast`: broadcast this transaction?
+   *    * If both `blocksBehind` and `expireSeconds` are present, then fetch the block which is `blocksBehind` behind head block, use it as a reference for TAPoS, and expire the transaction `expireSeconds` after that block's time.
+   * @returns node response if `broadcast`, `{signatures, serializedTransaction}` if `!broadcast`
+   */
   async transact(transaction: any, { broadcast = true, blocksBehind, expireSeconds }: TransactionConfig = {}): Promise<any> {
     let info: GetInfoResult;
 
@@ -201,6 +336,9 @@ export class Api {
     return pushTransactionArgs;
   }
 
+  /**
+   * Broadcast a signed transaction
+   */
   async pushSignedTransaction({ signatures, serializedTransaction }: PushTransactionArgs): Promise<any> {
     return this.rpc.push_transaction({
       signatures,
