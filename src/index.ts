a7b6cedd1c0ee36de59111545a3ad6df335ce7cb
Randy Torres <torresr14@gmail.com>
2018-09-17 08:25:18 -0700

    Add abi to json conversion method to api

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "babel-polyfill";
 import { Api } from "./eosjs2-api";
 import * as Rpc from "./eosjs2-jsonrpc";
 import SignatureProvider from "./eosjs2-jssig";
+import * as Serialize from "./eosjs2-serialize";
 
-export { Api, SignatureProvider, Rpc };
-export default { Api, SignatureProvider, Rpc };
+export { Api, SignatureProvider, Rpc, Serialize };
+export default { Api, SignatureProvider, Rpc, Serialize };
