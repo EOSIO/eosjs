ce174361c08e94030ce9154f7b047270934d342c
Cody Douglass <cody.douglass@block.one>
2018-10-09 18:42:02 -0400

    testing another format named object import

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ import * as Rpc from "./eosjs-jsonrpc";
 import SignatureProvider from "./eosjs-jssig";
 import * as Serialize from "./eosjs-serialize";
 
-export Api, SignatureProvider, Rpc, Serialize;
+export { Api, SignatureProvider, Rpc, Serialize };
