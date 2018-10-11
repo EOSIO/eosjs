4eca698a8f6360dc176d74b396350e42c8303f2c
Cody Douglass <cody.douglass@block.one>
2018-10-11 16:44:34 -0400

    added interfaces to index export process

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import "babel-polyfill";
 import { Api } from "./eosjs-api";
+import * as ApiInterfaces from "./api-interfaces"
 import * as Rpc from "./eosjs-jsonrpc";
+import * as RpcInterfaces from "./jsonrpc-interfaces"
 import SignatureProvider from "./eosjs-jssig";
 import * as Serialize from "./eosjs-serialize";
 
-export { Api, SignatureProvider, Rpc, Serialize };
+export { Api, ApiInterfaces, SignatureProvider, Rpc, RpcInterfaces, Serialize };
