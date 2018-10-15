81f051c6d954a39b8ee340b3e649b212c71ad70f
Cody Douglass <cody.douglass@block.one>
2018-10-15 15:51:16 -0400

    fixing test imports to match JsSignatureProvider named export

--- src/tests/eosjs-jssig.test.ts
+++ src/tests/eosjs-jssig.test.ts
@@ -1,6 +1,6 @@
 import * as ecc from "eosjs-ecc";
 
-import JsSignatureProvider from "../eosjs-jssig";
+import { JsSignatureProvider } from "../eosjs-jssig";
 
 describe("JsSignatureProvider", () => {
     const privateKeys = ["key1", "key2", "key3"];
