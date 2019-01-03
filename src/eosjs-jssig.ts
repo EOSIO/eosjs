a2e1eae8065c13a4b3a0520d4d24cfba692c910c
Jesse Abramowitz <jesseabramowitz@hotmail.com>
2019-01-03 15:19:43 -0500

    changed export default to module.export in eosjs-jssig.ts

--- src/eosjs-jssig.ts
+++ src/eosjs-jssig.ts
@@ -8,7 +8,7 @@ import { SignatureProvider, SignatureProviderArgs } from "./eosjs-api-interfaces
 import { convertLegacyPublicKey } from "./eosjs-numeric";
 
 /** Signs transactions using in-process private keys */
-export default class JsSignatureProvider implements SignatureProvider {
+class JsSignatureProvider implements SignatureProvider {
     /** map public to private keys */
     public keys = new Map<string, string>();
 
@@ -40,3 +40,5 @@ export default class JsSignatureProvider implements SignatureProvider {
         return { signatures, serializedTransaction };
     }
 }
+
+module.exports = JsSignatureProvider;
