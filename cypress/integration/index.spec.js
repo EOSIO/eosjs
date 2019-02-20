83e885252660205cbc9ff1d7c1805009022815ae
Cody Douglass <cody.douglass@block.one>
2019-02-20 13:20:25 -0500

    somewhat functional cypress test, seems to finish without second test succeeded

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,5 +1,12 @@
 describe('eosjs web test', () => {
   it('loads', () => {
+    cy.visit(('./src/tests/web.html'));
+  });
+  it('runs all tests successfully', () => {
     cy.visit(('./src/tests/web.html'))
+    cy.get('div.tests>div>button').each((test) => {
+      cy.wrap(test).click();
+      cy.contains('Success', { timeout: 5000 })
+    });
   });
 })
