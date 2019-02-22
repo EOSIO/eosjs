fe6990621720e1e367ea1291db39a260bfd7a0c4
Johan Nordberg <its@johan-nordberg.com>
2019-02-22 15:41:22 +0100

    Export numeric utils on Serializer

--- src/eosjs-serialize.ts
+++ src/eosjs-serialize.ts
@@ -1108,3 +1108,5 @@ export function deserializeAction(contract: Contract, account: string, name: str
         data: deserializeActionData(contract, account, name, data, textEncoder, textDecoder),
     };
 }
+
+export { numeric };
