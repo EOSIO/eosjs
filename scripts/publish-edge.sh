36daefd49892a5261224bcb37cf41a7744a40ab7
Mike Creque <mike.creque@block.one>
2019-02-25 15:54:50 -0500

    uses build number to avoid incorrect edge versioning

--- scripts/publish-edge.sh
+++ scripts/publish-edge.sh
@@ -10,17 +10,14 @@ setup_git
 echo "Creating new version"
 git checkout -- .
 
-git status 
+git status
 
-# get the short commit hash to include in the npm package
-current_commit="$(git rev-parse --short HEAD)";
-
-npm version prerelease -preid "${current_commit}" -no-git-tag-version
+npm version prerelease -preid "${TRAVIS_BUILD_NUMBER}" -no-git-tag-version
 
 git commit -a -m "Updating version [skip ci]"
 
 echo "Publish to NPM"
 
-cp .npmrc.template $HOME/.npmrc 
+cp .npmrc.template $HOME/.npmrc
 
 npm publish --tag edge
