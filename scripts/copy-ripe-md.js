27234892833465bc87f1dbfe2d20d24e77fc023b
Stuyk <stuykgaming@gmail.com>
2019-10-09 12:59:11 -0600

    Fix Copy File Pathing

--- scripts/copy-ripe-md.js
+++ scripts/copy-ripe-md.js
@@ -1,3 +1,7 @@
 var fs = require('fs');
-var root = __dirname + '\\..\\';
+var root = __dirname.replace('scripts', '');
+
+if(!fs.existsSync(root + 'dist'))
+    fs.mkdirSync(root + 'dist');
+
 fs.copyFileSync(root + 'src\\ripemd.es5.js', root + 'dist\\ripemd.js');
