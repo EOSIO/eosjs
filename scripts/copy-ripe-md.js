a95338d499e8451938c3ec4a04cbee6ea5d2a974
Stuyk <stuykgaming@gmail.com>
2019-10-09 13:16:19 -0600

    Use path.join for path resolving

--- scripts/copy-ripe-md.js
+++ scripts/copy-ripe-md.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
+var path = require('path');
 var root = __dirname.replace('scripts', '');
 
-if(!fs.existsSync(root + 'dist'))
-    fs.mkdirSync(root + 'dist');
+if(!fs.existsSync(path.join(root + 'dist')))
+    fs.mkdirSync(path.join(root + 'dist'));
 
-fs.copyFileSync(root + 'src\\ripemd.es5.js', root + 'dist\\ripemd.js');
+fs.copyFileSync(path.join(root + 'src/ripemd.es5.js'), path.join(root + 'dist/ripemd.js'));
