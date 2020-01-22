1800e0e986736bc7d12a001533c9a93b8b4ac9b5
Bradley Hart <bradley.hart-ext@block.one>
2020-01-22 15:59:09 -0500

    Reducing the amount of elliptic constructions
    Moving constructElliptic to a central location

--- src/PublicKey.ts
+++ src/PublicKey.ts
@@ -5,33 +5,32 @@ import {
     publicKeyToString,
     stringToPublicKey,
 } from './eosjs-numeric';
-import { Signature } from './Signature';
-import { PrivateKey } from './PrivateKey';
+import { constructElliptic, PrivateKey, Signature } from './eosjs-key-conversions';
 
 /** Represents/stores a public key and provides easy conversion for use with `elliptic` lib */
 export class PublicKey {
     constructor(private key: Key, private ec: EC) {}
 
     /** Instantiate public key from an EOSIO-format public key */
-    public static fromString(publicKeyStr: string): PublicKey {
+    public static fromString(publicKeyStr: string, ec?: EC): PublicKey {
         const key = stringToPublicKey(publicKeyStr);
-        return new PublicKey(key, constructElliptic(key.type));
+        if (!ec) {
+            ec = constructElliptic(key.type);
+        }
+        return new PublicKey(key, ec);
     }
 
     /** Instantiate public key from an `elliptic`-format public key */
-    public static fromElliptic(publicKey: EC.KeyPair, keyType: KeyType): PublicKey {
+    public static fromElliptic(publicKey: EC.KeyPair, keyType: KeyType, ec?: EC): PublicKey {
         const x = publicKey.getPublic().getX().toArray();
         const y = publicKey.getPublic().getY().toArray();
+        if (!ec) {
+            ec = constructElliptic(keyType);
+        }
         return new PublicKey({
             type: keyType,
             data: new Uint8Array([(y[31] & 1) ? 3 : 2].concat(x)),
-        }, constructElliptic(keyType));
-    }
-
-    /** Retrieve the public key from a private key */
-    public static fromPrivateKey(privateKey: PrivateKey): PublicKey {
-        const ellipticPrivateKey = privateKey.toElliptic();
-        return PublicKey.fromElliptic(ellipticPrivateKey, privateKey.getType());
+        }, ec);
     }
 
     /** Export public key as EOSIO-format public key */
@@ -57,26 +56,4 @@ export class PublicKey {
         const validationObj = ellipticPublicKey.validate();
         return validationObj.result;
     }
-
-    /** Recover a public key from a message digest and signature */
-    public static recover(digest: BNInput, signature: Signature, encoding?: string): PublicKey {
-        const ec = constructElliptic(signature.getType());
-        const ellipticSignature = signature.toElliptic();
-        const recoveredPublicKey = ec.recoverPubKey(
-            digest,
-            ellipticSignature,
-            ellipticSignature.recoveryParam,
-            encoding
-        );
-        const ellipticKPub = ec.keyFromPublic(recoveredPublicKey);
-        return PublicKey.fromElliptic(ellipticKPub, signature.getType());
-    }
 }
-
-/** Construct the elliptic curve object based on key type */
-const constructElliptic = (type: KeyType): EC => {
-    if (type === KeyType.k1) {
-        return new EC('secp256k1') as any;
-    }
-    return new EC('p256') as any;
-};
