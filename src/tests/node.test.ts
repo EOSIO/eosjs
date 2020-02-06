2b4562d47ca56ee9ea81027f2614855070d2c31e
Bradley Hart <bradley.hart-ext@block.one>
2020-02-06 16:37:01 -0500

    Resolving 'duplicate transaction' during tests

--- src/tests/node.test.ts
+++ src/tests/node.test.ts
@@ -5,6 +5,10 @@ describe('Node JS environment', () => {
     let transactionSignatures: any;
     let failedAsPlanned: boolean;
 
+    beforeEach(async () => {
+        await new Promise(resolve => setTimeout(resolve, 500));
+    });
+
     it('transacts with configuration object containing blocksBehind', async () => {
         transactionResponse = await tests.transactWithConfig({ blocksBehind: 3, expireSeconds: 30 });
         expect(Object.keys(transactionResponse)).toContain('transaction_id');
