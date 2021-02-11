e48402184aa542d3f9b103ea73bd9374fbc22458
Brad Hart <brad.l.hart@outlook.com>
2021-02-11 10:38:56 -0500

    Adding cross-platform rm -rf for package.json

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -22,6 +23,9 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ['**/*'] })
+    ],
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
     },
